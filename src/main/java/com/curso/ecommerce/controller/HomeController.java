e78b81901d0ddb6c521bb9df41e09d492c614561
stbn27 <estebanjosejulian@gmail.com>
2023-05-15 13:01:40 -0600

    Show the product details view to the user

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.curso.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.service.ProductoService;
@@ -11,6 +14,8 @@ import com.curso.ecommerce.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoService;
@@ -21,4 +26,11 @@ public class HomeController {
 		return "usuario/home";
 	}
 	
+	@GetMapping("/detallesProducto/{id}")
+	public String ProductoHome(@PathVariable Integer id) {
+		
+		log.info("Id producto enviado como parametro {}",id);
+		
+		return "usuario/productohome";
+	}
 }
